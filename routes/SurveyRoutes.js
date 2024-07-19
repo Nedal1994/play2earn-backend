b7035ba1f450fbcdb05ccc61e4d226397d67e184
Sanchusri Kavitha Babu <sanchu2804kb@gmail.com>
2024-07-19 16:46:49 +0100

    Add survey models

---
+++ routes/SurveyRoutes.js
@@ -0,0 +1,12 @@
+// const express = require('express');
+// const router = express.Router();
+// const surveyController = require('../controllers/cryptoSurveyController');
+
+// router.post('/', surveyController.createSurvey);
+// router.get('/', surveyController.getAllSurveys);
+// router.get('/:id', surveyController.getSurveyById);
+// router.put('/:id', surveyController.updateSurvey);
+// router.delete('/:id', surveyController.deleteSurvey);
+// router.post('/bulk-delete', surveyController.bulkDeleteSurveys);
+
+// module.exports = router;
