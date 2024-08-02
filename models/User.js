41d3a355a87a0425ed8b3d812dc8fa1d6896b313
Sanchusri Kavitha Babu <sanchu2804kb@gmail.com>
2024-08-02 16:28:37 +0100

    add fetch leaderboard api

--- models/User.js
+++ models/User.js
@@ -6,11 +6,15 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: true,
         unique: true,
+        trim: true,
     },
     email: {
         type: String,
         required: true,
         unique: true,
+        lowercase: true,
+        match: [/^\S+@\S+\.\S+$/, 'Please provide a valid email address'],
+
     },
     password: {
         type: String,
